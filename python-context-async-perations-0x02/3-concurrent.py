f7e7851137a74248e15dd975b0c9a045eb27bf21
siya <siyangwenya13@gmail.com>
2025-08-06 12:15:49 +0000

    2. Concurrent Asynchronous Database Queries

--- python-context-async-perations-0x02/3-concurrent.py
+++ python-context-async-perations-0x02/3-concurrent.py
@@ -1,1 +1,4 @@
 3-concurrent.py doesn't contain: ["import aiosqlite", "connect", "execute"]
+3-concurrent.py doesn't contain: ["async def async_fetch_users()", "async def async_fetch_older_users()", "return", "await"]
+3-concurrent.py doesn't contain: ["await asyncio.gather"]
+3-concurrent.py doesn't contain: ["fetch_concurrently()"]
